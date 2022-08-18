4b604800f50cf1b69db3556067207b1434ea8ca2
Sławomir Dziurman <g1.slawomir.dziurman@gmail.com>
2022-08-18 09:33:44 +0200

    favicon added

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const HOST_ADDRESS = "http://localhost:3001";      // bez slasha na końcu!!!
+export const HOST_ADDRESS = "http://localhost:9000";      // bez slasha na końcu!!!
 export const LOCAL_STORAGE_PREFIX = "animark-";
