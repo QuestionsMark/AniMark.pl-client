6d2903ba179047d97c7d06ad9d45fe54c5a5ccd6
Sławomir Dziurman <g1.slawomir.dziurman@gmail.com>
2022-10-05 17:23:17 +0200

    added lightbox to anime page

--- src/components/views/anime/anime-page/AnimeGaleryImageElement.tsx
+++ src/components/views/anime/anime-page/AnimeGaleryImageElement.tsx
@@ -1,16 +1,24 @@
 import { AnimeImage } from "../../../../types";
 import { Image } from "../../../common/Image";
+import { LightBoxActions } from "../../../common/LightBox";
 
 interface Props {
     img: AnimeImage;
+    actions?: LightBoxActions;
 }
 
-export const AnimeGaleryImageElement = ({ img }: Props) => {
-
+export const AnimeGaleryImageElement = ({ img, actions }: Props) => {
     const { fromAnime, src } = img;
 
+    const handleClick = () => {
+        if (!actions || actions?.index === undefined) return;
+        const { open, changeIndex, index } = actions;
+        changeIndex(index);
+        open();
+    };
+
     return (
-        <li className="anime-page__images-item">
+        <li className="anime-page__images-item" onClick={handleClick}>
             <Image alt={fromAnime} src={src} className="img--natural img--radius" />
         </li>
     );
